ea06bd1bd3a775218a26948e09b037d7239b835a
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-30 19:27:20 +0800

    edit app name

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import Button from "@/components/ui/Button";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
-import { FC } from "react";
 
 const page = async ({}) => {
   const session = await getServerSession(authOptions);
