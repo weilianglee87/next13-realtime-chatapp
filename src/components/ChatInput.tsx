c93a7bc8145378f3a7f6c4bb9a2fd06b67877b49
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-06-06 15:38:25 +0800

    text area created

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { FC, useRef, useState } from "react";
+import TextareaAutoSize from "react-textarea-autosize";
+interface ChatInputProps {
+  chatPartner: User;
+}
+
+const ChatInput: FC<ChatInputProps> = ({ chatPartner }) => {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [input, setInput] = useState<string>("");
+
+  const sendMessage = () => {};
+
+  return (
+    <div className='border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0'>
+      <div className='relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600'>
+        <TextareaAutoSize
+          ref={textareaRef}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              sendMessage();
+            }
+          }}
+          rows={1}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={`message ${chatPartner.name}`}
+          className='block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm sm:leading-6'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChatInput;
