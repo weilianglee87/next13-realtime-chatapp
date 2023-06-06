657098e701e9b83fd812bd6421cd078bb8d54edf
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-06-06 11:33:44 +0800

    ffriend chat created

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -8,6 +8,8 @@ import Image from "next/image";
 import SignOutButton from "@/components/SignOutButton";
 import FriendRequestsSidebarOptions from "@/components/FriendRequestsSidebarOptions";
 import { fetchRedis } from "@/helpers/redis";
+import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
+import SidebarChatList from "@/components/SidebarChatList";
 
 interface LayoutProps {
   children: ReactNode;
@@ -33,6 +35,8 @@ const Layout = async ({ children }: LayoutProps) => {
   const session = await getServerSession(authOptions);
   if (!session) notFound();
 
+  const friends = await getFriendsByUserId(session.user.id);
+
   const unseenRequestCount = (
     (await fetchRedis(
       "smembers",
@@ -46,12 +50,18 @@ const Layout = async ({ children }: LayoutProps) => {
         <Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
           <Icons.Logo className='h-8 w-auto text-indigo-600' />
         </Link>
-        <div className='text-xs font-semibold leading-6 text-gray-400'>
-          Your Chats
-        </div>
+
+        {friends.length > 0 ? (
+          <div className='text-xs font-semibold leading-6 text-gray-400'>
+            Your Chats
+          </div>
+        ) : null}
+
         <nav className='flex flex-1 flex-col'>
           <ul role='list' className='flex flex-1 flex-col gap-y-7'>
-            <li>//chats that this user has</li>
+            <li>
+              <SidebarChatList sessionId={session.user.id} friends={friends} />
+            </li>
             <li>
               <div className='text-xs font-semibold leading-6 text-gray-400'>
                 Overview
@@ -73,14 +83,13 @@ const Layout = async ({ children }: LayoutProps) => {
                     </li>
                   );
                 })}
+                <li>
+                  <FriendRequestsSidebarOptions
+                    sessionId={session.user.id}
+                    initialUnseenRequestCount={unseenRequestCount}
+                  />
+                </li>
               </ul>
-            </li>
-
-            <li>
-              <FriendRequestsSidebarOptions
-                sessionId={session.user.id}
-                initialUnseenRequestCount={unseenRequestCount}
-              />
             </li>
 
             <li className='-mx-6 mt-auto flex items-center'>
---
+++ src/components/SidebarChatList.tsx
@@ -0,0 +1,48 @@
+import { chatHrefConstructor } from "@/lib/utils";
+import { Session } from "inspector";
+import { usePathname, useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
+
+interface SidebarChatListProps {
+  friends: User[];
+  sessionId: string;
+}
+
+const SidebarChatList: FC<SidebarChatListProps> = ({ friends, sessionId }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [unseenMessages, setUnseenMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    if (pathname?.includes("chat")) {
+      setUnseenMessages((prev) => {
+        return prev.filter((msg) => !pathname.includes(msg.senderId));
+      });
+    }
+  }, [pathname]);
+
+  return (
+    <ul role='list' className='max-h-[25rem] overflow-y-auto -mx-2 space-y-1'>
+      {friends.sort().map((friend) => {
+        const unseenMessagesCount = unseenMessages.filter((unseenMsg) => {
+          return unseenMsg.senderId === friend.id;
+        }).length;
+
+        return (
+          <li key={friend.id}>
+            <a
+              href={`/dashboard/chat/${chatHrefConstructor(
+                sessionId,
+                friend.id
+              )}`}
+            >
+              hello
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default SidebarChatList;
