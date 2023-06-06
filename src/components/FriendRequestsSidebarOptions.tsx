f25e23d1f9f85be63085432fbab7da3ec2273fad
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-06-06 14:19:27 +0800

    add friend done

--- src/components/FriendRequestsSidebarOptions.tsx
+++ src/components/FriendRequestsSidebarOptions.tsx
@@ -20,7 +20,7 @@ const FriendRequestsSidebarOptions: FC<FriendRequestsSidebarOptionsProps> = ({
   return (
     <Link
       href='/dashboard/requests'
-      className='text-grey-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-seimibold'
+      className='text-grey-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
     >
       <div className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
         <User className='h-4 w-4' />
--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { chatHrefConstructor } from "@/lib/utils";
 import { Session } from "inspector";
 import { usePathname, useRouter } from "next/navigation";
@@ -35,8 +37,14 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ friends, sessionId }) => {
                 sessionId,
                 friend.id
               )}`}
+              className='text-gray hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
             >
-              hello
+              {friend.name}
+              {unseenMessagesCount > 0 ? (
+                <div className='bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center'>
+                  {unseenMessagesCount}
+                </div>
+              ) : null}
             </a>
           </li>
         );
