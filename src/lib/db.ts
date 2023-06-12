c212d725b2dea08ddc1dd50ee7ccdb1c3f5a40eb
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-06-12 15:37:42 +0800

    edit redis as string

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,6 @@
 import { Redis } from "@upstash/redis";
 
-export const db = Redis.fromEnv();
+export const db = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL as string,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+});
